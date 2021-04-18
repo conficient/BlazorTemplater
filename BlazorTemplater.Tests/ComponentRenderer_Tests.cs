5ae7103ec47c2b4f54c1b9a61fb973c1dd2f45ab
Howard Richards <dev@conficient.com>
2021-04-18 09:23:23 +0100

    Added fluent ComponentRenderer type - fixes #12

---
+++ BlazorTemplater.Tests/ComponentRenderer_Tests.cs
@@ -0,0 +1,211 @@
+﻿using BlazorTemplater.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BlazorTemplater.Tests
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [TestClass]
+    public class ComponentRenderer_Tests
+    {
+        [TestMethod]
+        public void Ctor_Test()
+        {
+            var builder = new ComponentRenderer<Simple>();
+
+            Assert.IsNotNull(builder);
+        }
+
+        #region Simple render
+
+        /// <summary>
+        /// Render a component (no service injection or parameters)
+        /// </summary>
+        [TestMethod]
+        public void Simple_Test()
+        {
+            const string expected = @"<b>Jan 1st is 2021-01-01</b>";
+            var actual = new ComponentRenderer<Simple>()
+                .Render();
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion Simple render
+
+        #region Parameters
+
+        /// <summary>
+        /// Test a component with a parameter
+        /// </summary>
+        [TestMethod]
+        public void ComponentBuilder_Parameters_Test()
+        {
+            // expected output
+            const string expected = "<p>Steve Sanderson is awesome!</p>";
+
+            var model = new TestModel()
+            {
+                Name = "Steve Sanderson",
+                Description = "is awesome"
+            };
+
+            var html = new ComponentRenderer<Parameters>()
+                .Set(c => c.Model, model)
+                .Render();
+
+            // trim leading space and trailing CRLF from output
+            var actual = html.Trim();
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Test a component with a parameter
+        /// </summary>
+        [TestMethod]
+        public void ComponentBuilder_Parameters_TestHtmlEncoding()
+        {
+            // expected output
+            const string expected = "<p>Safia &amp; Pranav are awesome too!</p>";
+
+            var templater = new Templater();
+            var model = new TestModel()
+            {
+                Name = "Safia & Pranav",    // the text here is HTML encoded
+                Description = "are awesome too"
+            };
+            var html = new ComponentRenderer<Parameters>()
+                .Set(c => c.Model, model)
+                .Render();
+
+            // trim leading space and trailing CRLF from output
+            var actual = html.Trim();
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Test a component with a parameter which isn't set
+        /// </summary>
+        [TestMethod]
+        public void ComponentBuilder_Parameters_TestIfModelNotSet()
+        {
+            // expected output
+            const string expected = "<p>No model!</p>";
+
+            var html = new ComponentRenderer<Parameters>()
+                .Render();
+
+            // trim leading space and trailing CRLF from output
+            var actual = html.Trim();
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion Parameters
+
+        #region Errors
+
+        /// <summary>
+        /// Test rendering model with error (null reference is expected)
+        /// </summary>
+        [TestMethod]
+        public void ComponentRenderer_Error_Test()
+        {
+            var templater = new Templater();
+
+            // we should get a NullReferenceException thrown as Model parameter is not set
+            Assert.ThrowsException<NullReferenceException>(() =>
+            {
+                _ = new ComponentRenderer<ErrorTest>().Render();
+            });
+        }
+
+        #endregion Errors
+
+        #region Dependency Injection
+
+        [TestMethod]
+        public void AddService_Test()
+        {
+            // set up
+            const int a = 2;
+            const int b = 3;
+            const int c = a + b;
+            string expected = $"<p>If you add {a} and {b} you get {c}</p>";
+
+            // fluent ComponentBuilder approach
+            var actual = new ComponentRenderer<ServiceInjection>()
+                .AddService<ITestService>(new TestService())
+                .Set(p => p.A, a)
+                .Set(p => p.B, b)
+                .Render();
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+        #region Nesting
+
+        /// <summary>
+        /// Test that a component containing other components render correctly
+        /// </summary>
+        [TestMethod]
+        public void ComponentRenderer_Nested_Test()
+        {
+            // expected output
+            // the spaces before the <p> come from the Parameters.razor component
+            // on Windows the string contains \r\n and on unix it's just \n
+            string expected = $"<b>Jan 1st is 2021-01-01</b>{Environment.NewLine}    <p>Dan Roth is cool!</p>";
+
+            var templater = new Templater();
+            var model = new TestModel()
+            {
+                Name = "Dan Roth",
+                Description = "is cool"
+            };
+            var html = new ComponentRenderer<NestedComponents>()
+                .Set(c => c.Model, model)
+                .Render();
+
+            // trim leading space and trailing CRLF from output
+            var actual = html.Trim();
+
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+        #region Cascading Values
+
+        [TestMethod]
+        public void ComponentRenderer_CascadingValues_Test()
+        {
+            const string expected = "<p>The name is Bill</p>";
+            var info = new CascadeInfo() { Name = "Bill" };
+
+            var html = new ComponentRenderer<CascadeParent>()
+                .Set(c => c.Info, info)
+                .Render();
+
+            // trim leading space and trailing CRLF from output
+            var actual = html.Trim();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        #endregion
+
+    }
+}
--- Docs/Usage.md
+++ Docs/Usage.md
@@ -6,6 +6,51 @@ Create templates using `.razor` components in your code, e.g. `MyComponent.razor
 
 They default to the namespace of the folder they are in, but you can override this with the [`@namespace` directive](https://docs.microsoft.com/en-us/aspnet/core/blazor/components/?view=aspnetcore-5.0#namespaces).
 
+### Fluent `ComponentRenderer`
+
+Create an instance of the `ComponentRenderer<T>` class, where `T` is the component type.
+
+Then call either `.AddService()` to add services for dependency injection, or `.Set()` to set parameters on the component. You can call these multiple times for different services and parameters.
+
+The final call is `.Render()` which renders the component as a HTML string.
+
+#### Simple Render
+To render a component which does not need any parameters set, you use the following code:
+```c#
+var html = new ComponentRenderer<MyComponent>().Render();
+```
+#### Setting Component Parameters
+
+Parameters are set using the `.Set()` method and a lambda function using the component to indicate the name:
+```c#
+var html = new ComponentRenderer<MyComponent>()
+            .Set(c => c.Model, myModel)
+            .Set(c => c.Title, "My title")
+            .Render();
+```
+#### Injecting Service Dependencies
+
+You can use [Dependency Injection](https://docs.microsoft.com/en-us/aspnet/core/blazor/fundamentals/dependency-injection) in Razor Components with the `@inject` directive. `ComponentRenderer` has a service container inside to provide these to the component, and you add these with `AddService()`
+
+```c#
+var html = new ComponentRenderer<MyComponent>()
+            .AddService<IServiceType>(serviceInstance)
+            .AddService(anotherService)
+            .Render();
+```
+BlazorTemplater supports two overloads to `AddService`, as shown above. The first uses a contract type (`IServiceType`) plus an instance which is a different class that implements the contract; `serviceInstance` in the example. The second method injects a service value and the contract is inferred as the same as the value.
+
+In ASP.NET Core it's possible to use DI to chain dependencies via constructor injection. That isn't supported here and you need to instantiate your services manually.
+
+#### Errors
+
+Razor Components are classes that execute code, so if there is an error in your component, `Render()` will throw an exception. A common error is not setting parameters resulting in a `NullReferenceException`.
+
+### `Templater` Class
+
+The initial versions of BlazorTemplater used the `Templater` class. I've retained the instructions for that here for reference. It's used by `ComponentRenderer` so it's essentially the same, but doesn't have the fluent interface.
+
+#### `Templater`
 Create an instance of the `Templater` class. This is a rendering host that also acts as a service container. This instance can be reused multiple times provided the services to be injected are the same.
 
 Use the `.RenderComponent<Type>(..)` method to generate the HTML.
@@ -59,7 +104,3 @@ In ASP.NET Core it's possible to use DI to chain dependencies via constructor in
 
 Razor Components are classes that execute code, so if there is an error in your component, `RenderComponent` will throw an exception. A common error is not setting parameters resulting in a `NullReferenceException`.
 
-### Supported Features
-
-#### Nesting Components
-Razor Components can be nested so you can use a more structured approach to designing your layout. 
